4c9778171bc65eec32b1c93497441d13b630b1f2
Fabrice Bellingard <fabrice.bellingard@sonarsource.com>
2016-10-07 08:43:05 +0200

    Move from Gulp plugin to Npm module

---
+++ index.js
@@ -0,0 +1,11 @@
+var fs = require('fs');
+var path = require('path');
+var exec = require('child_process').exec;
+var os = require('os');
+var format = require('util').format;
+
+module.exports = function (options) {
+
+    console.log("Pouet");
+
+};
