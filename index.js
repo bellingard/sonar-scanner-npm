330ff5b624f7c6fd96f4988c6d306a55872ec099
Fabrice Bellingard <fabrice.bellingard@sonarsource.com>
2016-10-07 14:14:38 +0200

    Read default values from package.json file

--- index.js
+++ index.js
@@ -4,47 +4,95 @@ var exec = require('child_process').exec;
 var os = require('os');
 var format = require('util').format;
 var extend = require('extend');
+var readPackage = require('read-pkg');
+var slug = require('slug');
 var log = require('fancy-log');
 var logError = log.error;
 
-module.exports = function (options) {
-    log("Starting SonarQube analysis...");
+/*
+ * Try to be smart and guess most SQ parameters from JS files that
+ * might exist - like "package.json".
+ */
+function defineSonarQubeScannerParams() {
+  var projectBaseDir = process.cwd();
 
-    var sonarqubeScannerParams = {
-      "sonar.projectKey" : "test",
-      "sonar.sources" : "."
-    };
+  // set default values if nothing is found later
+  var projectKey = slug(path.basename(projectBaseDir));
+  var projectName = projectKey;
+  var projectVersion = "1.0";
+  var projectDescription = "No description.";
+  var projectHomePage = "";
 
-    var SONAR_SCANNER_COMMAND = "sonar-scanner"
-    //var SONAR_SCANNER_COMMAND = "echo $SONARQUBE_SCANNER_PARAMS"
-
-    var mergedEnv = {};
-    extend(mergedEnv, process.env, {
-      SONARQUBE_SCANNER_PARAMS : JSON.stringify(sonarqubeScannerParams)
-    });
-
-    var options_exec = {
-        env : mergedEnv,
-        // Increase the amount of data allowed on stdout or stderr
-        // (if this value is exceeded then the child process is killed).
-        // TODO: make this customizable
-        maxBuffer : 1024*1024
-    }
-
-    var scanner = exec(SONAR_SCANNER_COMMAND, options_exec, function () {});
-    scanner.stdout.on('data', function (c) {
-        log(c);
-    });
-    scanner.stderr.on('data', function (c) {
-        logError(c);
-    });
-    scanner.on('exit', function (code) {
-        if (code !== 0) {
-            logError(format('Return code: %d.', code));
-            // TODO: maybe we should throw an error so that upstream code can
-            // know that something went wrong
+  // now try to read "package.json" file
+  try {
+      var packageFile = path.join(projectBaseDir, "package.json");
+      fs.accessSync(packageFile, fs.F_OK);
+      var pkg = readPackage.sync(packageFile);
+      if (pkg) {
+        projectKey = slug(pkg.name);
+        projectName = pkg.name;
+        projectVersion = pkg.version;
+        if (pkg.description) {
+          projectDescription = pkg.description;
         }
-        log(format('Return code: %d.', code));
-    });
+        if (pkg.homepage) {
+          projectHomePage = pkg.homepage;
+        }
+      }
+  } catch (e) {
+      // No "package.json" file - let's remain on the defaults
+      log('No "package.json" file found. Using default settings.');
+  }
+
+  var sonarqubeScannerParams = {
+    "sonar.projectKey" : projectKey,
+    "sonar.projectName" : projectName,
+    "sonar.projectVersion" : projectVersion,
+    "sonar.projectDescription" : projectDescription,
+    "sonar.links.homepage" : projectHomePage,
+    "sonar.sources" : ".",
+    "sonar.exclusions" : "node_modules/**"
+  };
+
+  return sonarqubeScannerParams;
+}
+
+function scan(options) {
+  log("Starting SonarQube analysis...");
+
+  var SONAR_SCANNER_COMMAND = "sonar-scanner"
+  //var SONAR_SCANNER_COMMAND = "echo $SONARQUBE_SCANNER_PARAMS"
+
+  var mergedEnv = {};
+  extend(mergedEnv, process.env, {
+    SONARQUBE_SCANNER_PARAMS : JSON.stringify(defineSonarQubeScannerParams())
+  });
+
+  var options_exec = {
+      env : mergedEnv,
+      // Increase the amount of data allowed on stdout or stderr
+      // (if this value is exceeded then the child process is killed).
+      // TODO: make this customizable
+      maxBuffer : 1024*1024
+  }
+
+  var scanner = exec(SONAR_SCANNER_COMMAND, options_exec, function () {});
+  scanner.stdout.on('data', function (c) {
+      log(c);
+  });
+  scanner.stderr.on('data', function (c) {
+      logError(c);
+  });
+  scanner.on('exit', function (code) {
+      if (code !== 0) {
+          logError(format('Return code: %d.', code));
+          // TODO: maybe we should throw an error so that upstream code can
+          // know that something went wrong
+      }
+      log(format('Return code: %d.', code));
+  });
 
 };
+
+module.exports = scan;
+//module.exports = defineSonarQubeScannerParams;
