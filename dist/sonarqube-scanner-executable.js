867163185fe62d83d059fcb3460e92d25909afad
Fabrice Bellingard <fabrice.bellingard@sonarsource.com>
2016-10-20 15:45:55 +0200

    Start writing the download of exec file

--- dist/sonarqube-scanner-executable.js
+++ dist/sonarqube-scanner-executable.js
@@ -1,4 +1,9 @@
+var fs = require('fs');
+var path = require('path');
 var exec = require('child_process').execSync;
+var mkdirp = require('mkdirp').sync;
+var download = require('download');
+var log = require('fancy-log');
 
 module.exports = defineSonarQubeScannerExecutable;
 
@@ -13,15 +18,67 @@ function defineSonarQubeScannerExecutable() {
     command += ".bat";
   }
 
+  // #1 - Try to execute the "sonar-scanner" command
   try {
     exec(command + " -v", {});
+    // if we're here, this means that the SQ Scanner can be executed
+    // TODO: we should check that it's at least v2.8+
+    // let's return it
+    return command;
   } catch (e) {
     // sonar-scanner is not in the PATH => download the binaries for the
-    // correct platform... but this is not supported for the moment.
-    throw Error("SonarQube Scanner not found and impossible to download a compatible binary for it (yet!).");
+    // correct platform...
+    log("Trying to use the target binaries for the '" + process.platform + "' platform...");
   }
 
-  return command;
+  // #2 - Download the binaries from https://github.com/henryju/bdd-scanner-natif
+  if (isWindows() || isLinux() || isMac()) {
+    command = getPlatformBinaries();
+    if (command) {
+      return command;
+    }
+  }
+
+  // #3 - If we're here, nothing can be done...
+  throw Error("SonarQube Scanner not found and impossible to download a compatible binary for it.");
+}
+
+function getPlatformBinaries() {
+  var platformBinariesVersion = "0.1";
+  var installFolder = path.join(process.env.HOME, ".sonar", "native-sonar-scanner", platformBinariesVersion);
+  var binaryExtension = "";
+  if (isWindows()) {
+    binaryExtension = ".bat";
+  }
+
+  try {
+      var platformExecutable = path.join(installFolder, "bin", "org.sonarsource.scanner.standalone" + binaryExtension);
+      log("Checking if executable exists: " + platformExecutable);
+      fs.accessSync(platformExecutable, fs.F_OK);
+      // executable exists, let's return it
+      return platformExecutable;
+  } catch (e) {
+      // Folder does not exist - let's create it and install the binaries
+      log("Could not find executable. Proceed with download...");
+      log("Creating " + installFolder);
+      mkdirp(installFolder);
+      var targetOS = "mac";
+      var fileName = "sonarqube-scanner-"
+                      + targetOS
+                      + "-x86_64-"
+                      + platformBinariesVersion
+                      + ".zip";
+      var downloadUrl = "https://github.com/henryju/bdd-scanner-natif/releases/download/"
+                        + platformBinariesVersion + "/" + fileName;
+      log("Downloading from " + downloadUrl);
+      // download(downloadUrl).pipe(fs.createWriteStream(path.join(installFolder, fileName)));
+      download(downloadUrl, installFolder).then(() => {
+        log("DONE!");
+      });
+
+  }
+
+  return null;
 }
 
 /*
