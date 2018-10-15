e3b37ebe5d74bb56456dad1d98a421f25a110a56
raksit <rak-sit@hotmail.com>
2018-10-15 16:09:00 +0700

    :package: Updated gulpfile script for version 4

--- gulpfile.js
+++ gulpfile.js
@@ -4,25 +4,7 @@ var mocha = require('gulp-mocha')
 // Regular users will call 'require('sonarqube-scanner')' - but not here: eat your own dog food! :-)
 var sonarqubeScanner = require('./dist/index')
 
-gulp.task('default', ['test'], function(callback) {
-  // We just run a SonarQube analysis and push it to SonarCloud
-  // (No need to pass the server URL and the token, we're using the Travis
-  //  Addon for SonarCloud which does this for you.)
-  // ----------------------------------------------------
-  sonarqubeScanner(
-    {
-      options: {
-        'sonar.projectName': 'SonarQube/SonarCloud Scanner for the JavaScript world',
-        'sonar.sources': 'dist',
-        'sonar.tests': 'specs'
-      }
-    },
-    callback
-  )
-  // ----------------------------------------------------
-})
-
-gulp.task('test', ['pre-test'], function() {
+gulp.task('test', function() {
   let result = gulp
     .src(['specs/**/*.js'])
     .pipe(
@@ -45,4 +27,22 @@ gulp.task('pre-test', function() {
     // Force `require` to return covered files
     .pipe(istanbul.hookRequire())
   return result
+})
+
+gulp.task('default', gulp.series('pre-test', 'test'), function(callback) {
+  // We just run a SonarQube analysis and push it to SonarCloud
+  // (No need to pass the server URL and the token, we're using the Travis
+  //  Addon for SonarCloud which does this for you.)
+  // ----------------------------------------------------
+  sonarqubeScanner(
+    {
+      options: {
+        'sonar.projectName': 'SonarQube/SonarCloud Scanner for the JavaScript world',
+        'sonar.sources': 'dist',
+        'sonar.tests': 'specs'
+      }
+    },
+    callback
+  )
+  // ----------------------------------------------------
 })
