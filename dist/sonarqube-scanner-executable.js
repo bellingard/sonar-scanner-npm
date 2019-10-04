b772437dc6c397eea65928b44bb6af4371c16e66
Jacek <jacek.poreda@sonarsource.com>
2019-10-04 14:34:26 +0200

    Update Scanner CLI to 4.2.0.1873

--- dist/sonarqube-scanner-executable.js
+++ dist/sonarqube-scanner-executable.js
@@ -56,7 +56,7 @@ function prepareExecEnvironment(params, process) {
  * Returns the SQ Scanner executable for the current platform
  */
 function getSonarQubeScannerExecutable(passExecutableCallback) {
-  const platformBinariesVersion = '4.0.0.1744'
+  const platformBinariesVersion = '4.2.0.1873'
   var targetOS = findTargetOS()
   var installFolder = path.join(os.homedir(), '.sonar', 'native-sonar-scanner')
   var binaryExtension = ''
