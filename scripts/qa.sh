737aa363b4d87b7a247d880499a67b0b46341ed9
Tibor Blenessy <tibor.blenessy@sonarsource.com>
2023-02-09 10:49:39 +0100

    Fix build and QA (#35)
    * Update docker file
    
    * Fix build and QA

--- scripts/qa.sh
+++ scripts/qa.sh
@@ -9,7 +9,9 @@ source cirrus-env QA
 # TODO: https://sonarsource.atlassian.net/browse/BUILD-2397
 # We should have a virtual repo that include npmjs and sonarsource-npm-public-qa
 # and an other one that include npmjs and sonarsource-npm-public-builds
-# The repo npm is wrongly configured and should only inculde npmjs and sonarsource-npm-public-releases
+# The repo npm is wrongly configured and should only include npmjs and sonarsource-npm-public-releases
 npm config set registry https://repox.jfrog.io/artifactory/api/npm/npmjs/
 
-cd test && npm install "https://$ARTIFACTORY_PRIVATE_USERNAME:$ARTIFACTORY_PRIVATE_PASSWORD@repox.jfrog.io/artifactory/sonarsource-npm-public-qa/sonarqube-scanner/-/sonarqube-scanner-$VERSION-$BUILD_NUMBER.tgz"
+cd test/integration
+npm install
+npm install --no-save "https://$ARTIFACTORY_PRIVATE_USERNAME:$ARTIFACTORY_PRIVATE_PASSWORD@repox.jfrog.io/artifactory/sonarsource-npm-public-qa/sonarqube-scanner/-/sonarqube-scanner-$VERSION-$BUILD_NUMBER.tgz"
